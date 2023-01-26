2cc01e9b1bf89f7f0114e0da3abed974ad9e07b7
Jake Champion <jchampion@fastly.com>
2023-01-26 13:40:50 +0000

    remove assertions

--- getLottoNumbers.spec.js
+++ getLottoNumbers.spec.js
@@ -4,27 +4,9 @@ import * as assert from 'uvu/assert';
 import sinon from "sinon";
 
 test('should make a GET request to https://euro.lotto.game/results', async () => {
-    const fetchStub = sinon.stub();
-    const responseStub = {
-        json: sinon.stub()
-    };
-    const { getLottoNumbers } = await esmock('./getLottoNumbers.js', {
-        undici: { fetch: fetchStub.resolves(responseStub) }
-    });
-    await getLottoNumbers();
-    assert.equal(fetchStub.calledOnceWith('https://euro.lotto.game/results'), true);
 });
 
 test('should parse the response as JSON and then turn it into a Set', async () => {
-    const fetchStub = sinon.stub();
-    const responseStub = {
-        json: sinon.stub().resolves([1,2,3,4,5,6,7])
-    };
-    const { getLottoNumbers } = await esmock('./getLottoNumbers.js', {
-        undici: { fetch: fetchStub.resolves(responseStub) }
-    });
-    const result = await getLottoNumbers();
-    assert.equal(result, new Set([1,2,3,4,5,6,7]));
 });
 
 test.run();
